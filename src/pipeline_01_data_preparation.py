99091ba49f4bb135db7ef7f4f28defadbea0bcd1
TheCodecrafter-Mohnish <mohnishawade@gmail.com>
2022-04-27 15:42:51 +0200

    The config added

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -3,7 +3,7 @@ import argparse
 import yaml
 import logging
 
-'''
+
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
@@ -12,7 +12,8 @@ def read_params(config_path):
 def main(config_path, datasource):
     config = read_params(config_path)
     print(config)
-'''
+
+
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     default_config_path = os.path.join("config", "params.yaml")
@@ -20,4 +21,5 @@ if __name__=="__main__":
     args.add_argument("--datasource", default=None)
 
     parsed_args = args.parse_args()
-    print(parsed_args.config ,parsed_args.datasource)
+    #print(parsed_args.config ,parsed_args.datasource)
+    main(config_path=parsed_args.config, datasource=parsed_args.datasource)
